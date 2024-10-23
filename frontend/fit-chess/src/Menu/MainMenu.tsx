02920b10bde05b7d9f76b3434046d141773b3d65
Uzimonkey123 <normanbabiak@gmail.com>
2024-10-24 01:03:28 +0200

    feat: Basics for menu and submenus creation

---
+++ frontend/fit-chess/src/Menu/MainMenu.tsx
@@ -0,0 +1,22 @@
+import { Link } from 'react-router-dom';
+import './Menu.css';
+import chessIcon from '../assets/menu-icon.png';
+
+function MainMenu() {
+  return (
+    <div className="main-menu">
+      <div className="logo-container">
+        <img src={chessIcon} alt="Chess icon" className="icon" />
+        <h1 className="title">Chessnek</h1>
+        <img src={chessIcon} alt="Chess icon" className="icon" />
+      </div>
+      <div className="menu-buttons">
+        <Link to="/new-game">
+          <button>New Game</button>
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default MainMenu;
